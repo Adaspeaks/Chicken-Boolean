7380ce42e76c772c5167e329d387f3648580c6a1
Joseph Manning <jgmanning3@yahoo.com>
2021-07-09 14:37:27 -0700

    Build search area on homepage.handlebar. Create navigation.handlebar

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -1,24 +1,27 @@
 const router = require('express').Router();
 const { User } = require('../models');
-const withAuth = require('../utils/auth');
+// const withAuth = require('../utils/auth');
 
-router.get('/', withAuth, async (req, res) => {
-  try {
-    const userData = await User.findAll({
-      attributes: { exclude: ['password'] },
-      order: [['name', 'ASC']],
-    });
+// router.get('/', withAuth, async (req, res) => {
+  router.get('/', (req, res)=>{
+    res.render('homepage');
+  })
+  // try {
+  //   const userData = await User.findAll({
+  //     attributes: { exclude: ['password'] },
+  //     order: [['name', 'ASC']],
+  //   });
 
-    const users = userData.map((project) => project.get({ plain: true }));
+  //   const users = userData.map((project) => project.get({ plain: true }));
 
-    res.render('homepage', {
-      users,
-      logged_in: req.session.logged_in,
-    });
-  } catch (err) {
-    res.status(500).json(err);
-  }
-});
+  //   res.render('homepage', {
+  //     users,
+  //     logged_in: req.session.logged_in,
+  //   });
+  // } catch (err) {
+  //   res.status(500).json(err);
+  // }
+// });
 
 router.get('/login', (req, res) => {
   if (req.session.logged_in) {
