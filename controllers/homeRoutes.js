7f1e217d4074fa597e605f6c94dd2f8271e51c0a
Adaspeaks <adaspeaks@gmail.com>
2021-07-11 16:27:11 -0700

    added routes for different pages

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -1,32 +1,42 @@
-const router = require('express').Router();
-const { User } = require('../models');
-const withAuth = require('../utils/auth');
+const router = require("express").Router();
+const { User } = require("../models");
+const withAuth = require("../utils/auth");
 
-router.get('/', withAuth, async (req, res) => {
+router.get("/", async (req, res) => {
+  res.render("homepage");
+});
+
+router.get("/login", (req, res) => {
+  if (req.session.logged_in) {
+    res.redirect("/");
+    return;
+  }
+
+  res.render("login");
+});
+
+router.get("/signup", (req, res) => {
+  if (req.session.logged_in) {
+    res.redirect("/");
+    return;
+  }
+  res.render("signup");
+});
+
+router.get("/mypage", withAuth, async (req, res) => {
   try {
-    const userData = await User.findAll({
-      attributes: { exclude: ['password'] },
-      order: [['name', 'ASC']],
+    const userData = await User.findByPk(req.session.user_id, {
+      attributes: { exclude: [`password`] },
+      include: [{ model: recipe }],
     });
+    const user = userData.get({ plain: true });
 
-    const users = userData.map((project) => project.get({ plain: true }));
-
-    res.render('homepage', {
-      users,
-      logged_in: req.session.logged_in,
+    res.render("mypage", {
+      ...user,
+      logged_in: true,
     });
   } catch (err) {
     res.status(500).json(err);
   }
 });
-
-router.get('/login', (req, res) => {
-  if (req.session.logged_in) {
-    res.redirect('/');
-    return;
-  }
-
-  res.render('login');
-});
-
 module.exports = router;
