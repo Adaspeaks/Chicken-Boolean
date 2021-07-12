0c1f5b9afa4de0988533135e0f7b00c55ce11cca
Adaspeaks <adaspeaks@gmail.com>
2021-07-12 14:10:44 -0700

    Added search query functionality

--- controllers/api/spoonacular.js
+++ controllers/api/spoonacular.js
@@ -1,19 +1,36 @@
-const router = require('express').Router();
-const { User, Steps, Recipe } = require('../../models');
+const router = require("express").Router();
+const { User, Steps, Recipe } = require("../../models");
+const axios = require("axios");
 
 document.querySelector("#searchBtn").addEventListenter("click", getRecipe);
+// vegSEL
+router.get("/:search", async (req, res) => {
+  try {
+    const recipeSearch = await Recipe(
+      title = req.params.title,
+       id = req.params.id,
+      vegetartian = req.params.is_vegetarian,
+      vegan = req.params.is_vegan,
+      gluten_free = req.params.is_glutenFree,
+      dairy_free = req.params.is_dairyFree
+    );
 
-router.get("/:id", async (req, res) => {
-    try {
-        const recipeTitle = await Recipe(req.params.id, req.params.is_vegetarian, req.params.is_vegan, req.params.is_glutenFree, req.params.is_dairyFree);
-    
-        if (!recipeTitle) {
-          res.status(404).json({ message: "No recipe available" });
-          return;
-        }
-    
-        res.status(200).json(recipeTitle);
-      } catch (err) {
-        res.status(500).json(err);
-      }
-    });
+    const dietdropdown = document.getElementById(vegSEL);
+    const diet = dietdropdown.options[dietdropdown.selectedindex].text;
+
+    const dairyFree = document.querySelector(`#dairyFree:checked`).val = "dairy,";
+    const glutenFree = document.querySelector(`#glutenFree:checked`).val = "gluten,";
+
+    const searchTerm = document.getElementById(`searchArea`).val;
+
+    const recipeData = await axios.get(`https://api.spoonacular.com/recipes/complexSearch?apiKey=${process.env.SPOON_API}&addRecipeInformation=true&diet=${diet}&intolerances=${dairyFree}${glutenFree}&query=${searchTerm}`)
+    if (!recipeTitle) {
+      res.status(404).json({ message: "No recipe available" });
+      return;
+    }
+
+    res.status(200).json(recipeSearch);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
