ae0c38a38bc0fc2ebad62c945d6ff808ea8d17b6
Adaspeaks <adaspeaks@gmail.com>
2021-07-12 20:36:32 -0700

    created search function on frontend to hook

---
+++ public/js/search.js
@@ -0,0 +1,30 @@
+const formdata = { searchButton = document.getElementById("#searchBtn"),
+ searchField = document.getElementById("#recipeSearch"),
+
+ dietdropdown = document.getElementById(vegSel),
+ diet = dietdropdown.options[dietdropdown.selectedindex].text,
+
+ dairyFree = (document.querySelector(`#dairyFree:checked`).val = "dairy,"),
+ glutenFree = (document.querySelector(`#glutenFree:checked`).val = "gluten,"),
+ searchTerm = req.params.search,
+};
+ const configObj ={
+  method: "POST",
+  headers: { "content-type": "application/json", acccept: "application/json" },
+  body: JSON.stringify(formdata),
+};
+
+fetch("/api/search/", configObj)
+.then(function(response){
+    return response.json();
+})
+.then (function(object){
+    console.log(object);
+})
+searchButton.addEventListener("click", function () {
+  if (searchField.value == "") {
+    console.log("no search query found");
+  } else {
+    window.location.replace(`/search${searchField.value}`);
+  }
+});
