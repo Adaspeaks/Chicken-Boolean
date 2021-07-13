509412118f33e44183e1e31187a6df964647e2d5
Adaspeaks <adaspeaks@gmail.com>
2021-07-12 20:36:51 -0700

    required spoonacular file

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -1,6 +1,8 @@
 const router = require('express').Router();
 const userRoutes = require('./userRoutes');
+const spoonacular = require('./spoonacular')
 
 router.use('/users', userRoutes);
+router.use(`/search`, spoonacular);
 
 module.exports = router;
