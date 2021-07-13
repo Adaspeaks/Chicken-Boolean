78151d6f044348278cd9ca1256d7f415357e1cdb
Adaspeaks <adaspeaks@gmail.com>
2021-07-13 03:07:29 -0700

    added sign up (needs work)

---
+++ public/js/createAcc.js
@@ -0,0 +1,24 @@
+const createAccBtn = document.getElementById("createBtn", signupFormHandler);
+
+const signupFormHandler = async (e) => {
+  e.preventDefault();
+
+  const name = document.getElementById("username").value.trim();
+  const password = document.getElementById("password").value.trim();
+
+  if (name && password) {
+      const response = await fetch("/api/users/signup", {
+        method: "POST",
+        body: JSON.stringify({
+          name,
+          password,
+        }),
+        headers: { "content-Type": "application/json" },
+        });
+        if (response.ok) {
+            document.location.replace('/');
+        } else{
+            alert(response.statusText);
+        }
+    };
+  };
