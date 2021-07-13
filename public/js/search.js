4dba4671763034a61c53aca15b38255f43a387c6
Adaspeaks <adaspeaks@gmail.com>
2021-07-12 23:11:25 -0700

    spoonacular search works

--- public/js/search.js
+++ public/js/search.js
@@ -1,33 +1,13 @@
-const formdata = { searchButton = document.getElementById("#searchBtn"),
- searchField = document.getElementById("#recipeSearch"),
+const search = document.querySelector("#recipeSearch");
+const searchButton = document.querySelector("#searchBtn");
 
- dietdropdown = document.getElementById(vegSel),
- diet = dietdropdown.options[dietdropdown.selectedindex].text,
-
- dairyFree = (document.querySelector(`#dairyFree:checked`).val = "dairy,"),
- glutenFree = (document.querySelector(`#glutenFree:checked`).val = "gluten,"),
- searchTerm = req.params.search,
-};
- const configObj ={
-  method: "GET",
-  headers: { "content-type": "application/json", acccept: "application/json" },
-  body: JSON.stringify(formdata),
-};
-
-fetch("/api/search/", configObj)
-.then(function(response){
-    return response.json();
-})
-.then (function(object){
-    console.log(object);
-})
-.catch(function(error){
-    console.log(error.message);
+searchButton.addEventListener("click", function(){
+    let searchInput = document.getElementById("recipeSearch").value;
+    getRecipes(searchInput);
+    
 });
-searchButton.addEventListener("click", function () {
-  if (searchField.value == "") {
-    console.log("no search query found");
-  } else {
-    window.location.replace(`/search${searchField.value}`);
-  }
-});
+
+const getRecipes = async (searchTerm) => {
+    let response = await fetch(`api/search/${searchTerm}`);
+    response = await response.json();
+};
